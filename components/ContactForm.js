7e01cd2c99da03572c57b0959a145b8266a18e27
Helder Fonseca <helder2391@gmail.com>
2022-12-07 11:17:54 -0100

    add background image and change form description

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -4,11 +4,11 @@ const ContactForm = () => {
 
   return (
         <Fragment>
-            <h1 className="text-left md:px-0 text-5xl font-bold md:inline-block py-2 rounded-md text-[#FBED04]">
+            <h1 className="text-left md:px-0 text-5xl font-bold md:inline-block py-2 rounded-md text-[#FFF200]">
                 Faça o seu negócio chegar a mais de 4000 clientes com um único email! 
             </h1>
-            <h3 className="text-[1rem] mb-8 md:w-[100%] md:text-left text-center">
-                Preencha os campos abaixo para obter a nossa oferta especial Black Friday
+            <h3 className="text-[1rem] mb-8 md:w-[100%] md:text-left font-bold">
+                Preencha os campos abaixo para obter uma oferta especial de 30% de desconto
             </h3>
             <div id="mlb2-5795093" className="ml-form-embedContainer ml-subscribe-form ml-subscribe-form-5795093">
                 <div className="ml-form-align-center">
--- components/Hero.js
+++ components/Hero.js
@@ -12,7 +12,7 @@ export default function Hero() {
                 <ContactForm />
                 </div>
                 <div className="md:w-1/2 w-full self-center">
-                    {/*<h1 className="text-5xl font-bold bg-[#FBED04] md:inline-block p-2 rounded-md text-[#070707]">E-mail Marketing</h1>
+                    {/*<h1 className="text-5xl font-bold bg-[#FFF200] md:inline-block p-2 rounded-md text-[#070707]">E-mail Marketing</h1>
                     <h2 className="text-4xl md:text-left italic font-light pt-3">Faça o seu negócio chegar a <span className="font-bold">mais de 4000 clientes</span> com um único email!</h2>
                     <div className="pt-8">
                         <p className="leading-relaxed text-justify">
@@ -27,7 +27,7 @@ export default function Hero() {
                             económicas e ricas em conversões da atualidade.
                         </p>
                 </div>*/}
-                    <Image src={heroImage} alt="Black Friday" className="object-contain" layout="responsive" priority/>
+                    {/*<Image src={heroImage} alt="Black Friday" className="object-contain" layout="responsive" priority/>*/}
                 </div>
             </div>
         </div>
